397775c4fb7e3b7e7a289832c206c34043de6ae4
Dedy Purnama <128173163+dedypurnama712@users.noreply.github.com>
2026-08-27 11:30:01 +0700

    test
    test

---
+++ main.go
@@ -0,0 +1,28 @@
+﻿package main
+
+import (
+"fmt"
+"net/http"
+"os"
+)
+
+var version = "dev"
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "Hello, DevOps! version=%s\n", version)
+}
+
+func main() {
+http.HandleFunc("/", rootHandler)
+
+port := os.Getenv("PORT")
+if port == "" {
+port = "8080"
+}
+
+fmt.Println("listening on :" + port)
+
+if err := http.ListenAndServe(":"+port, nil); err != nil {
+fmt.Println(err)
+}
+}
